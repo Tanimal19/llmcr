ad32602a4bc43b2f4150311ffa4e54f3a4261d26
bob cheng <bobcheng.bob@gmail.com>
2026-05-27 17:01:07 +0800

    remove unused files

--- spring-app/run_system_test.sh
+++
@@ -1,6 +0,0 @@
-TIMESTAMP=$(date +%Y%m%d-%H%M%S)
-LOG_FILE="./logs/system-test-$TIMESTAMP.log"
-
-./mvnw spring-boot:run \
-    -Dspring-boot.run.arguments="--app.mode=api --config.path=./config.test.yml" \
-    2>&1 | tee "$LOG_FILE"
